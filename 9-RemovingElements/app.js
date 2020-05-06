de0212102d84b8eb2a8eed70c12a028703f8c8db
tomcevans@live.co.uk <tomcevans24@gmail.com>
2020-05-06 15:40:02 +0100

    this is my commit comment

--- 9-RemovingElements/app.js
+++ 9-RemovingElements/app.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('input');
 const button = document.getElementById('submit');
 /*add new const below*/
+const removeBtn = document.getElementById('remove');
 
 /*from previous ex*/
 button.addEventListener('click', () => {
@@ -15,3 +16,10 @@ button.addEventListener('click', () => {
 })
 
 /*New code below*/
+removeBtn.addEventListener(`click`,() => {
+
+    let lastItem = document.querySelector(`li:last-child`);
+    let list = document.getElementsByTagName('ul')[0];
+
+    list.removeChild(lastItem);
+})
